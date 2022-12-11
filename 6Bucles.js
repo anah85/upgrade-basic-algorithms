ae420319f26a35d8ba4223dd4547679b9f2140d7
Ana Hartmann <ana.hartmann@protonmail.com>
2022-12-11 12:34:04 +0100

    first Iteración #6

---
+++ 6Bucles.js
@@ -0,0 +1,30 @@
+//1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+        for(i=0;i<=9;i++){
+        console.log(i);
+        }
+
+//1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo cuando el resto del numero dividido entre 2 sea 0.
+for(i=0; i<10; i++){
+    if(i % 2 == 0);
+    console.log(i);
+}
+
+//1.3 Crea un bucle para conseguir dormir contando ovejas. 
+for(i=1;i<=30;i++){
+    console.log(i + ' ovejita');
+    }
+
+//Este bucle empieza en 0 y termina en 10. 
+for(i=0;i<=10;i++){
+    console.log(i);
+    }
+
+    /*Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle y 
+    cambia el mensaje en la última vuelta a 'Dormido!' */
+    for(i=0; i<=5; i++){
+        if(i<=4){
+        console.log('Intentando dormir');
+    }else{
+            console.log('Dormido!'); 
+        }
+        }
